25829c98960b1467b0fbb7e46ff5629d81c99875
madospare <hundepopp@gmail.com>
2021-09-29 12:08:33 +0200

    Something

---
+++ 2IMIgame/Assets/Player/scripts/PlayerPos.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+
+    private GameMaster gm;
+
+    public GameObject player;
+
+    void Start()
+    {
+
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+
+    }
+
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            player.transform.position = gm.lastCheckPointPos;
+        }
+
+    }
+
+}
