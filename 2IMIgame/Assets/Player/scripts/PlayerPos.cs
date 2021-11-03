0b40c241b577e87e6251adc99c704841095fa0b7
madospare <hundepopp@gmail.com>
2021-11-03 09:05:19 +0100

    Fixed checkpoints

--- 2IMIgame/Assets/Player/scripts/PlayerPos.cs
+++ 2IMIgame/Assets/Player/scripts/PlayerPos.cs
@@ -31,9 +31,12 @@ public class PlayerPos : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
 
-        if (collisionInfo.collider.tag == ("Enemy"))
+        if (collisionInfo.collider.tag == ("Respawn"))
         {
             gm.lastCheckPointPos = transform.position;
+        } else if (collisionInfo.collider.tag == ("Enemy"))
+        {
+            player.transform.position = gm.lastCheckPointPos;
         }
 
     }
