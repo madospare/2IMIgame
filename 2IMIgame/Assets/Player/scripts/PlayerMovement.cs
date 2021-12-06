ee4c484f44970f8dff0768e14ba5d0dade73ae94
madospare <hundepopp@gmail.com>
2021-12-06 11:02:46 +0100

    ladders

--- 2IMIgame/Assets/Player/scripts/PlayerMovement.cs
+++ 2IMIgame/Assets/Player/scripts/PlayerMovement.cs
@@ -13,6 +13,13 @@ public class PlayerMovement : MonoBehaviour
     float horizontalMove = 0f;
     bool jump = false;
 
+    public Rigidbody2D rb;
+
+    public float distance;      // For climbing ladders
+    public LayerMask Ladder;
+    public bool isClimbing;
+    float verticalMove = 0f;
+
     PhotonView view;
 
     private void Start()
@@ -47,6 +54,31 @@ public class PlayerMovement : MonoBehaviour
             // Move character
             controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
             jump = false;
+
+            RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, Ladder);
+
+            if (hitInfo.collider != null)
+            {
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    isClimbing = true;
+                }
+            } else
+            {
+                isClimbing = false;
+            }
+
+            if(isClimbing == true && hitInfo.collider != null)
+            {
+                verticalMove = Input.GetAxisRaw("Vertical");
+                rb.velocity = new Vector2(rb.position.x, verticalMove * runSpeed);
+                rb.gravityScale = 0;
+                runSpeed = 10f;
+            } else
+            {
+                rb.gravityScale = 3;
+                runSpeed = 40f;
+            }
         }
 
     }
